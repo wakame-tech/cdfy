73c71aa1b34ade749e8333db6a2f3e7b43a3ae58
wakamewakame-tech <kamata1919@gmail.com>
2023-09-30 14:11:56 +0900

    Remove deno server

--- server/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from './deps.ts'
-import { io } from './socket.ts'
-
-await serve(io.handler(), {
-  port: 8080,
-  onListen({ port, hostname }) {
-    console.log(`Server started at http://${hostname}:${port}`)
-  },
-})
