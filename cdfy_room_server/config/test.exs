a264678fbe1f41343ab0c5994eea47ad21f4966c
wakamewakame-tech <kamata1919@gmail.com>
2024-01-14 10:51:17 +0900

    chore: mix phx.new

---
+++ cdfy_room_server/config/test.exs
@@ -0,0 +1,33 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :cdfy_room_server, CdfyRoomServer.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "cdfy_room_server_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :cdfy_room_server, CdfyRoomServerWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "wGW4FJ1NJSStd5C66g1nw1xwUh5j5FytXQCZrX1yX9bGIk0SQUcJ6R1Q263vSafS",
+  server: false
+
+# In test we don't send emails.
+config :cdfy_room_server, CdfyRoomServer.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
---
+++ cdfy_room_server/lib/cdfy_room_server_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule CdfyRoomServerWeb.PageHTML do
+  use CdfyRoomServerWeb, :html
+
+  embed_templates "page_html/*"
+end
