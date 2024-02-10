fc0e1a81c4ccbc7d3e0ac1515947e0f420918997
wakamewakame-tech <kamata1919@gmail.com>
2024-02-10 12:11:45 +0900

    fix: setup dev db

--- cdfy_room_server/config/dev.exs
+++ cdfy_room_server/config/dev.exs
@@ -6,6 +6,7 @@ config :cdfy_room_server, CdfyRoomServer.Repo,
   password: "postgres",
   hostname: "localhost",
   database: "cdfy_room_server_dev",
+  port: 5434,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
