bf02534e73ff0dd24f431f8a365be125cea23519
wakame-tech <kamata1919@gmail.com>
2024-06-27 21:27:04 +0900

    chore: mix phx.gen.auth

--- cdfy/lib/cdfy_web/router.ex
+++ cdfy/lib/cdfy_web/router.ex
@@ -1,6 +1,8 @@
 defmodule CdfyWeb.Router do
   use CdfyWeb, :router
 
+  import CdfyWeb.UserAuth
+
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
@@ -8,6 +10,7 @@ defmodule CdfyWeb.Router do
     plug(:put_root_layout, html: {CdfyWeb.Layouts, :root})
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug(:fetch_current_user)
   end
 
   pipeline :api do
@@ -15,8 +18,7 @@ defmodule CdfyWeb.Router do
   end
 
   scope "/", CdfyWeb do
-    pipe_through(:browser)
-
+    pipe_through [:browser, :require_authenticated_user]
     live("/", HomeLive)
     get("/plugins", PluginController, :index)
     get("/plugins/:id", PluginController, :show)
@@ -43,6 +45,44 @@ defmodule CdfyWeb.Router do
 
       live_dashboard("/dashboard", metrics: CdfyWeb.Telemetry)
       forward("/mailbox", Plug.Swoosh.MailboxPreview)
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", CdfyWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{CdfyWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", CdfyWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{CdfyWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", CdfyWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{CdfyWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
